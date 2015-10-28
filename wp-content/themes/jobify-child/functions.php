d533229e24abc5960cea7d6cc90c3641bbf19e0f
SAB <dev@santaanablvd.com>
2015-10-27 23:25:59 -0500

    more updates from UAT list to child functions.php

--- wp-content/themes/jobify-child/functions.php
+++ wp-content/themes/jobify-child/functions.php
@@ -14,7 +14,7 @@ function jobify_child_styles() {
 add_action( 'wp_enqueue_scripts', 'jobify_child_styles', 20 );
 
 /**
-* Plugin Name: Jobify - Disable Default Google Fonts and Enqueue Custom
+* Disable Default Google Fonts and Enqueue Custom
 */
 function custom_jobify_fonts() {
 wp_dequeue_style( 'jobify_fonts' );
@@ -23,6 +23,18 @@ wp_enqueue_style( 'jobify-custom-fonts', 'https://fonts.googleapis.com/css?famil
 }
 add_action( 'wp_enqueue_scripts', 'custom_jobify_fonts', 20 );
 
+
+/**
+* Change City / Town to just City on Checkout form
+*/
 add_filter( 'woocommerce_default_address_fields', 'my_default_address_fields' ); 
 
 function my_default_address_fields( $fields ) { $fields['city']['label'] = 'City'; $fields['city']['placeholder'] = 'City'; return $fields; }
+
+
+/**
+* Changes on Job Listing Form
+*/
+add_filter( 'submit_job_form_fields', 'custom_submit_job_form_fields' );
+
+function custom_submit_job_form_fields( $fields ) { $fields['job']['application']['label'] = 'Send Application To This Email Address or Application URL'; $fields['job']['application']['placeholder'] = 'Email address or URL where application is to be sent'; $fields['job']['job_location']['label'] = 'Address of Job Location'; $fields['job']['job_location']['placeholder'] = 'Complete address of where the job is located';     $fields['job']['job_location']['required'] = true; $fields['job']['job_location']['description'] = ''; return $fields; }
